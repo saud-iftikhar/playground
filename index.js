c68fe00745e2b125a0f646c530fa0d959b069147
saudiftikhar39@gmail.com <saudiftikhar39@gmail.com>
2023-07-20 15:30:15 +0500

    changes

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-import React from 'react'
-
-export const index = () => {
-  return (
-    <div>index</div>
-  )
-  }
+ import React from 'react'
+ 
+ const index = () => {
+   return (
+       <div>index</div>
+       <div>index</div>
+       <div>index</div>
+       )
+ }
+ 
+ export default index
