fafc1b914e6c6008cf3b0d98bbed16d1c93f60fa
saud iftikhar <107649311+saud-iftikhar@users.noreply.github.com>
2025-04-23 10:53:50 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+const express = require("express");
+require("dotenv/config")
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/user")
+var cors = require('cors')
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const PORT = process.env.PORT || 3001
 
-// Register the service worker
-serviceWorkerRegistration.register();
+const app = express();
+
+const corsOptions = {
+    exposedHeaders: 'Authorization',
+  }
+
+app.use(cors(corsOptions));
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.bpksr.mongodb.net/?retryWrites=true&w=majority`, ()=> console.log('Connected to DB'));
+
+app.use(express.json());
+
+app.use("/api/user/", userRoutes);
+
+app.get("/", (req, res) => {
+    res.send("Testing...")
+
+}) 
+
+
+
+app.listen(PORT, ()=> console.log(`API Server: ${PORT}`));
+
+
+
